8102846db8011cb143e15e868d73623b807ae2f1
Gabriel Pasquale <pasqualeunq@gmail.com>
2021-05-21 17:55:26 -0300

    dron deacceleration

--- Levels/Level2/entities/player/Dron.gd
+++ Levels/Level2/entities/player/Dron.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-var speed = 9
+var speed = 11
 
-var FRICTION_WEIGHT:float = 0.1
+var deacceleration:float = 0.8
+var FRICTION_WEIGHT:float = 0.3
 
 var container
 var game_over = false
@@ -18,16 +19,19 @@ func initialize(level_2):
 func get_input():
 	var right := Input.is_action_pressed("move_right")
 	var left := Input.is_action_pressed("move_left")
-	velocity.x += lerp(float(right) - float(left), 0.2, 0.2) * speed 
+	velocity.x += clamp(float(right) - float(left), -FRICTION_WEIGHT, FRICTION_WEIGHT) * speed
 	
 	var down := Input.is_action_pressed("move_down")
 	var up := Input.is_action_pressed("move_up")
-	velocity.y += lerp(float(down) - float(up), 0.2, 0.2) * speed 
+	velocity.y += clamp(float(down) - float(up), -FRICTION_WEIGHT, FRICTION_WEIGHT) * speed
 
 func _physics_process(_delta):
 	if !game_over:
 		if !container.control:
 			get_input()
+		else:
+			velocity.x = 0 if velocity.x < deacceleration and velocity.x > -deacceleration else velocity.x + deacceleration if velocity.x < 0 else velocity.x - deacceleration
+			velocity.y = 0 if velocity.y < deacceleration and velocity.y > -deacceleration else velocity.y + deacceleration if velocity.y < 0 else velocity.y - deacceleration
 		velocity = move_and_slide(velocity, Vector2.ZERO)
 
 func hit():
