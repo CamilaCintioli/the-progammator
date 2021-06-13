a150927a8d12091d45550c1e792f0620b0d89551
Gabriel Pasquale <pasqualeunq@gmail.com>
2021-06-13 15:41:04 -0300

    number to start in checkpoints

--- gui/Menu.gd
+++ gui/Menu.gd
@@ -5,28 +5,38 @@ var container
 func _process(_delta):
 	if Input.is_action_pressed("start"):
 		get_tree().change_scene("res://menu/PlayerMenu.tscn")
+	if Input.is_action_pressed("one"):
+		button_1()
+	if Input.is_action_pressed("two"):
+		button_2()
+	if Input.is_action_pressed("three"):
+		button_3()
+	if Input.is_action_pressed("four"):
+		button_4()
+	if Input.is_action_pressed("five"):
+		button_5()
 
 func initialize(_container):
 	container = _container
 	play()
 
-func _on_Button1_pressed():
+func button_1():
 	CheckpointsMenu.set_check(1)
 	container.start_level_1()
 
-func _on_Button2_pressed():
+func button_2():
 	CheckpointsMenu.set_check(2)
 	container.start_level_1()
 
-func _on_Button3_pressed():
+func button_3():
 	CheckpointsMenu.set_check(3)
 	container.start_level_1()
 
-func _on_Button4_pressed():
+func button_4():
 	CheckpointsMenu.set_check(4)
 	container.start_level_1()
 
-func _on_Button5_pressed():
+func button_5():
 	CheckpointsMenu.set_check(5)
 	container.start_level_1()
 
